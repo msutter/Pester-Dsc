539b7d286f4163b164901a8dccbf3ba78be1e299
Marc Sutter <marc.sutter@swisscom.com>
2015-03-08 20:09:45 +0100

    first commit

---
+++ Pester-Dsc.psm1
@@ -0,0 +1,47 @@
+﻿Function Get-DscTestEnvirement {
+    param (
+        [String]$TestDrive,
+        [String]$ResourcePath
+    )
+    $TestDrivePath        = (Get-item $TestDrive).FullName.TrimEnd('/').TrimEnd('\')
+    $ModulePath           =  Split-Path -Parent $ResourcePath | Split-Path -Parent
+    $ModulesRootPath      = $ModulePath | Split-Path -Parent
+    $ModuleName           = (Split-Path -Leaf $ModulePath)
+    $ResourceName         = (Split-Path -Leaf $ResourcePath)
+    $SchemaMofPath        = "${ResourcePath}\${ResourceName}.schema.mof"
+    $MofRootPath          = "${TestDrivePath}\TestConfig"
+    $MofPath              = "${MofRootPath}\test.mof"
+    $TestModulePath       = "${TestDrivePath}\Modules"
+    $OriginalPSMODULEPATH = $env:PSMODULEPATH
+
+    if (Test-path $TestModulePath) {Remove-Item -Recurse -Force $TestModulePath}
+    if (Test-path $MofRootPath) {Remove-Item -Recurse -Force $MofRootPath}
+
+    if (!(Test-path $TestModulePath)) {New-Item -ItemType directory -Path $TestModulePath}
+    if (!(Test-path $MofRootPath)) {New-Item -ItemType directory -Path $MofRootPath}
+
+    Copy-Item -Recurse "${ModulePath}" "${TestModulePath}\${ModuleName}"
+    $env:PsModulePath = "${TestModulePath};C:\Windows\system32\WindowsPowerShell\v1.0\Modules"    
+    Get-DscResource -Name $ResourceName | out-null
+    . ".\${ResourceName}.Tests.Config.ps1"
+    TestConfig -OutputPath $MofRootPath
+
+    $env:PSMODULEPATH = $OriginalPSMODULEPATH
+
+    $DscProperties = @{
+        'TestDrivePath'   = $TestDrivePath;
+        'SchemaMofPath'   = $SchemaMofPath;
+        'MofPath'         = $MofPath;
+        'ResourcePath'    = $ResourcePath;
+        'ModulePath'      = $ModulePath;
+        'ModulesRootPath' = $ModulesRootPath;
+        'ModuleName'      = $ModuleName;
+        'ResourceName'    = $ResourceName
+        }
+
+    $DscTest = New-Object -TypeName PSObject -Prop $DscProperties 
+    return $DscTest
+
+
+
+}
